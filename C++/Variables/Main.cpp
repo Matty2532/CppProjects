13330a7a8dcc6f95baf5caaac4fcc137cd9ba3b4
Matt Young <Matty2532@hotmail.com>
2014-04-15 14:27:13 +0100

    Initial Variables project commit
    Initial commit of the Variables C++ project

---
+++ C++/Variables/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int a = 6;
+	cout << a << endl;
+	int b = 4;
+	int c = 21;
+	int sum = b + c;
+	cout << sum << endl;
+
+	cout << "Press enter to continue...";
+	std::cin.get();
+	return 0;
+}
--- README.md
+++ README.md
@@ -3,3 +3,4 @@ C++ Projects
 Initial commit  
 Initial commit of the BaseCode C++ project  
 Initial commit of the PrintLn C++ project  
+Initial commit of the Variables C++ project  
