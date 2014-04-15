090edc6ea86d8d6a3a488a8caf0c669feb36829a
Matt Young <Matty2532@hotmail.com>
2014-04-15 14:16:22 +0100

    Initial PrintLn project commit
    Initial commit of the PrintLn C++ project

---
+++ C++/PrintLn/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	cout << "Hello world" << endl;
+
+	cout << "Press enter to continue...";
+	std::cin.get();
+	return 0;
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ C++ Projects
 ============
 Initial commit  
 Initial commit of the BaseCode C++ project  
+Initial commit of the PrintLn C++ project  
